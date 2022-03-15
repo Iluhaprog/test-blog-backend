fcf0fc4cd71b2c05e432690abfe2329cfb1f0143
Ilya Novak <rickmortyand4@gmail.com>
2022-03-15 23:11:20 +0300

    feat: Added docs and auth for endpoint which upload file.

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -2,17 +2,40 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/JwtAuthGuard';
 import { FileService } from './file.service';
-
+import { FileUploadDto } from './interfaces/FileUploadDto';
+import { FileUploadResponseDto } from './interfaces/FileUploadResponseDto';
+@ApiTags('file')
 @Controller('file')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'File to upload.',
+    type: FileUploadDto,
+  })
+  @ApiOkResponse({
+    type: FileUploadResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized',
+  })
   uploadImage(@UploadedFile() file: Express.Multer.File) {
     return this.fileService.uploadFile(file);
   }
